6758ed53a0e260cb255c79b6ed8c998e9c5c9ce5
yunkehe <yunkehe@163.com>
2017-09-10 21:19:42 +0800

    const

--- ES6/chapters/let-const.js
+++ ES6/chapters/let-const.js
@@ -11,9 +11,23 @@ a[6]();
 // let x; //let 不会变量提升
 
 // 暂时性死区 temporal dead zone
-function bar(x=y, y=2){ //y还未声明
-    return [x, y];
-}
-bar();
+// function bar(x=y, y=2){ //y还未声明
+//     return [x, y];
+// }
+// bar();
 
 // 不允许重复声明
+function b(){
+    let a = 10;
+    // var a = 1;
+}
+
+// const 声明常量
+const PI = 3.1415;
+
+// PI = 3;
+// const obj = {'name': 'heke'};
+const obj = Object.freeze({'name': 'heke'});
+
+obj.name = 'wang';
+console.log(obj.name); 
