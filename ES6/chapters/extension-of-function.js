5f65f2fcad8d47f4fd190312fec6b60a31ab9a71
yunkehe <yunkehe@163.com>
2017-09-24 21:55:15 +0800

    function

--- ES6/chapters/extension-of-function.js
+++ ES6/chapters/extension-of-function.js
@@ -1,1 +1,22 @@
 // 函数的默认值
+function foo({x, y=1}) {
+    console.log('x, y: ', x, y);
+}
+
+foo({x: 5, y: 6});
+
+function m1({x = 0, y = 0} = {}){
+    return [x, y];
+}
+
+function m2({x, y} = {x: 0, y: 0}){
+    return [x, y];
+}
+
+console.log( m1({x: 3}) );
+console.log( m2({x: 3}) );
+
+// length属性返回没有指定默认值的参数个数
+console.log('length: ',  (function(a){}).length );
+console.log('length: ', (function(a=1){}).length );
+console.log('length: ', (function(a, b, c=1){}).length );
