e3dcee162b1054c12c833614583339a0348c1f57
何珂 <yunkehe@192.168.0.103>
2018-02-05 21:14:55 +0800

    generator

--- ES6/chapters/Generator.js
+++ ES6/chapters/Generator.js
@@ -45,3 +45,46 @@ const wrapped = wrapper(function* () {
 })
 
 wrapped().next('hello');
+
+// for...of循环 自动遍历Generator函数 且此时不再需要调用next方法
+function* fibonaci() {
+	let [prev, curr] = [0, 1];
+	for(;;){
+		[prev, curr] = [curr, prev + curr];
+		yield curr;
+	}
+}
+for(let n of fibonaci()){
+	if(n > 100) break;
+	console.log('n', n);
+}
+
+function* objectEntries() {
+	let propKeys = Object.keys(this);
+
+	for(let propKey of propKeys){
+		yield [propKey, this[propKey]];
+	}
+}
+
+let jane = {first: 'Jane', last: 'Doe'};
+jane[Symbol.iterator] = objectEntries;
+
+for( let [key, value] of jane){
+	console.log(`${key}: ${value}`);
+}
+
+// Generator.prototype.throw();
+// Generator.prototype.return();
+// 如果Gnerator函数内部有try...finally代码块，那么return方法会推迟到finally代码块执行完后再执行
+
+function* gen(){
+	yield 1;
+	yield 2;
+	yield 3;
+}
+
+var g = gen();
+console.log('gen: ', g.next());
+console.log('gen: ', g.return('foo'));
+console.log('gen: ', g.next());
